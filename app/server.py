00c76ffd0ec100162812488a35803f04a5a77765
Oreoluwa Banwo <oreban4u@gmail.com>
2025-07-06 19:57:15 -0500

    update origins

--- app/server.py
+++ app/server.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.router import router
+from app.settings import settings
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=settings.get_allowed_origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,17 @@ class Settings(BaseSettings):
     project_dir: str = PROJECT_DIR
     genai_client_api_key: str
     port: int = 9000
+    allowed_origins: str | None = None
 
     model_config = SettingsConfigDict(extra="ignore", env_file=f"{ROOT_DIR}/.env")
+
+    @property
+    def get_allowed_origins(self):
+        if self.allowed_origins is not None:
+            return self.allowed_origins.split("|")
+        if self.environment == "local":
+            return ["*"]
+        return []
+
 
 settings = Settings()
